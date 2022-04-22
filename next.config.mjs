331b96374cbe495aed36f957579ef0b42908f8ad
Klyshnikov Roman <klyshnikov.roman@gmail.com>
2022-04-22 19:44:33 +0300

    Настроил ApolloClient

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,25 @@ const nextConfig = {
   eslint: {
     dirs: ['src'],
   },
+  publicRuntimeConfig: {
+    graphqlEndpoint: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  },
+  experimental: {
+    modularizeImports: {
+      lodash: {
+        transform: 'lodash/{{member}}',
+      },
+    },
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader'
+    })
+
+    return config
+  },
 }
 
 export default nextConfig
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import getConfig from 'next/config'
+
+export const {
+  publicRuntimeConfig: { graphqlEndpoint },
+} = getConfig()
