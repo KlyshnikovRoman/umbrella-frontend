70d21a0d93e306313cdfcde4c0ab4c7f6fd09462
Klyshnikov Roman <klyshnikov.roman@gmail.com>
2022-05-11 16:03:34 +0300

    Создал общий компонент индикатор CapsLock'а

---
+++ src/components/caps-lock-indicator/caps-lock-indicator.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export interface CapsLockIndicatorProps {
+  onCapsLockChange?(bool: boolean): void
+  children: React.ReactNode | ((bool: boolean) => React.ReactNode)
+}
+
+export function CapsLockIndicator({ onCapsLockChange, children }: CapsLockIndicatorProps) {
+  const [isCapsLock, setIsCapsLock] = React.useState<boolean>(false)
+
+  const onKeyPress = React.useCallback(
+    (event: KeyboardEvent) => {
+      const capsLockState = event?.getModifierState('CapsLock')
+
+      if (isCapsLock !== capsLockState) {
+        setIsCapsLock(capsLockState)
+        onCapsLockChange?.(capsLockState)
+      }
+    },
+    [isCapsLock, onCapsLockChange]
+  )
+
+  React.useEffect(() => {
+    document.addEventListener('keydown', onKeyPress)
+    document.addEventListener('keyup', onKeyPress)
+
+    return () => {
+      document.removeEventListener('keydown', onKeyPress)
+      document.removeEventListener('keyup', onKeyPress)
+    }
+  }, [onKeyPress])
+
+  if (typeof children === 'function') {
+    return <>{children(isCapsLock)}</>
+  }
+
+  return <>{isCapsLock ? children : null}</>
+}
