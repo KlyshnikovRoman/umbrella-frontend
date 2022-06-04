92771022599a36cf704bc86dc48d432dfdad542e
Klyshnikov Roman <klyshnikov.roman@gmail.com>
2022-06-05 02:03:55 +0300

    Изменил метод обработчик onIconButtonClick и стиль кода в типе

--- src/components/password-input/password-input.tsx
+++ src/components/password-input/password-input.tsx
@@ -4,7 +4,7 @@ import { EndAdornment } from './components/end-adornment'
 
 export type PasswordInputProps = {
   isShown?: boolean
-  onShownChange?(bool: boolean): void
+  onShownChange?: (bool: boolean) => void
 } & TextFieldProps
 
 export const PasswordInput = React.forwardRef<any, PasswordInputProps>(function PasswordInput(
@@ -12,16 +12,16 @@ export const PasswordInput = React.forwardRef<any, PasswordInputProps>(function 
   ref
 ) {
   const { InputProps = {}, isShown: isShownProp, onShownChange, disabled, ...rest } = props
-  const [isShownState, setIsShownState] = React.useState<boolean>(false)
+  const [isShownState, setIsShownState] = React.useState(false)
   const isControlled = isShownProp !== undefined
   const isShown = isControlled ? isShownProp : isShownState
 
   const onIconButtonClick = React.useCallback(() => {
-    if (isControlled) {
-      onShownChange?.(!isShown)
-    } else {
+    if (!isControlled) {
       setIsShownState(!isShown)
     }
+
+    onShownChange?.(!isShown)
   }, [isControlled, onShownChange, isShown])
 
   InputProps.endAdornment = <EndAdornment isShown={isShown} onIconButtonClick={onIconButtonClick} />
